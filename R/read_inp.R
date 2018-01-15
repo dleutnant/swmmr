68df5df8fbd15a3a9f701db87c2b0be985175360
DoeringA <doering@hydromech.uni-hannover.de>
2018-01-15 18:21:04 +0100

    delete leading whitespaces in read_inp

--- R/read_inp.R
+++ R/read_inp.R
@@ -36,6 +36,9 @@ read_inp <- function(x, rm.comment = TRUE) {
   # read lines
   inp_lines <- readLines(x)
   
+  # delete leading whitespaces in strings
+  inp_lines <- gsub("^\\s+", "", inp_lines)
+  
   # find section start
   section_start <- grep("\\[", inp_lines, value = F)
 
