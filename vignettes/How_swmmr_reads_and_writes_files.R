2415d80c3873cde5c670c893e31d5824938c57fa
dleutnant <leutnant@fh-muenster.de>
2018-02-17 18:14:44 +0100

    update travis yml

---
+++ vignettes/How_swmmr_reads_and_writes_files.R
@@ -0,0 +1,12 @@
+## ----setup, include = FALSE----------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----inp_sec, echo = FALSE-----------------------------------------------
+swmmr:::input_sections
+
+## ----rpt_sec, echo = FALSE-----------------------------------------------
+swmmr:::report_sections
+
