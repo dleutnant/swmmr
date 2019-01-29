6ea091d29830366b218ad5347acdc7a3b38009eb
hsonne <hauke.sonnenberg@gmx.de>
2019-01-29 08:44:56 +0100

    Adapt test script

--- inst/extdata/test_read.R
+++ inst/extdata/test_read.R
@@ -8,8 +8,14 @@ out_file <- file.path(tempdir(), "swmm-Example1.out")
 
 swmmr::run_swmm(inp_file, out = out_file, exec = exec)
 
-swmmr:::OpenSwmmOutFile(out_file)
-swmmr:::CloseSwmmOutFile()
+#swmmr:::OpenSwmmOutFile(out_file)
+#swmmr:::CloseSwmmOutFile()
 
-swmmr::read_out(out_file, iType = 2, object_name = "1", vIndex = 0)
-swmmr:::GetSwmmResultPart(iType = 2, iIndex = 1, vIndex = 0, firstPeriod = 1, lastPeriod = 1)
+#swmmr::read_out(out_file, iType = 2, object_name = "1", vIndex = 0)
+args <- list(file = out_file, iType = 0, object_name = "1", vIndex = 0)
+result1 <- do.call(swmmr::read_out, args)
+result2 <- do.call(swmmr::read_out, c(args, firstPeriod = 2))
+
+swmmr:::GetSwmmResultPart(iType = 1, iIndex = 1, vIndex = 1, firstPeriod = 0, lastPeriod = 0)
+
+#swmmr:::GetSwmmResultPart(iType = 2, iIndex = 1, vIndex = 0, firstPeriod = 1, lastPeriod = 1)
