19866fd2254b7fb7b1e21a83099e3da204cc1034
hsonne <hauke.sonnenberg@gmx.de>
2019-02-02 10:29:22 +0100

    Extend test file

--- inst/extdata/test_read.R
+++ inst/extdata/test_read.R
@@ -5,6 +5,15 @@ inp_file <- package_file("extdata", "Example1.inp")
 exec <- "/home/hauke/CProgramming/Stormwater-Management-Model/bin/run-swmm"
 
 out_file <- file.path(tempdir(), "swmm-Example1.out")
+out_file <- "/home/hauke/Downloads/SWMM/result.out"
+
+file.size(out_file) / (1024^3)
+
+#result <- swmmr:::OpenSwmmOutFile(out_file)
+#swmmr:::CloseSwmmOutFile()
+
+result <- swmmr::read_out(out_file, byObject = FALSE, multiColumn = TRUE,
+                          firstPeriod = 100, lastPeriod = 200)
 
 swmmr::run_swmm(inp_file, out = out_file, exec = exec)
 
@@ -12,9 +21,17 @@ swmmr::run_swmm(inp_file, out = out_file, exec = exec)
 #swmmr:::CloseSwmmOutFile()
 
 #swmmr::read_out(out_file, iType = 2, object_name = "1", vIndex = 0)
-args <- list(file = out_file, iType = 0, object_name = c("1", "2"), vIndex = 0)
+args <- list(
+  file = out_file, 
+  iType = 0, 
+  object_name = c("SV8", "SV27", "SV34"), 
+  vIndex = c(0, 2, 4),
+  multiColumn = TRUE, 
+  byObject = FALSE
+)
 
-do.call(swmmr::read_out, args)
+result <- do.call(swmmr::read_out, args)
+
 do.call(swmmr::read_out, c(args, firstPeriod = 200))
 do.call(swmmr::read_out, c(args, firstPeriod = -1))
 do.call(swmmr::read_out, c(args, firstPeriod = 35, lastPeriod = 33))
