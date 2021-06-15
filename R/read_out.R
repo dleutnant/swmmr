2ede3faa3ae3fdac751a23a808a2c62c2fdda2b8
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2021-06-16 00:54:09 +0200

    Add get_out_(content|version)()
    that were added to dleutnant/swmmr in the
    meanwhile

--- R/read_out.R
+++ R/read_out.R
@@ -232,3 +232,62 @@ stop_if_out_of_range <- function(x, a, b)
     )
   }
 }
+
+#' Get the swmm version the .out file was generated with
+#' 
+#' This function opens an .out file and extract the swmm version the file was 
+#' generated with. It belongs to a set of helper functions which aim to simplify 
+#' the work with .out files. The lifecycle of this function is considered 
+#' experimental.
+#' 
+#' @inheritParams read_out
+#' @return A vector of type integer
+#' @examples
+#' \dontrun{
+#' version <- get_out_version("model.out")
+#' } 
+#' @rdname get_out_version
+#' @export
+get_out_version <- function(file = "") {
+  
+  # get the content
+  list_of_results <- get_out_content(file)
+  
+  # extract version information
+  version <- list_of_results$meta$version
+  
+  return(version)
+  
+}
+
+#' Get the content of an .out file.
+#' 
+#' This function opens an .out file and lists all available time series data.
+#' Currently, the list is returned 'as is' which might change in future. 
+#' It belongs to a set of helper functions which aim to simplify 
+#' the work with .out files. The lifecycle of this function is considered 
+#' experimental.
+#' 
+#' @inheritParams read_out
+#' @return A list showing the available content.
+#' @examples
+#' \dontrun{
+#' content <- get_out_content("model.out")
+#' } 
+#' @rdname get_out_content
+#' @export
+get_out_content <- function(file = "") {
+  
+  # open swmm out file
+  list_of_results <- OpenSwmmOutFile(outFile = file)
+  
+  # close swmm out file
+  on.exit(CloseSwmmOutFile())
+  
+  # check error
+  if (exists("error", list_of_results)) {
+    warning("error reading out file")
+  }
+  
+  return(list_of_results)
+}
