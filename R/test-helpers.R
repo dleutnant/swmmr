a76d3b4367669732b05dc35aaa42e8104bf3f4a2
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2023-01-08 06:51:39 +0100

    Add two more test helper functions
    - read_example_input_files()
    - existing_path_or_null()

--- R/test-helpers.R
+++ R/test-helpers.R
@@ -8,3 +8,27 @@ example_input_files <- function()
     mustWork = TRUE
   )
 }
+
+# read_example_input_files -----------------------------------------------------
+#' @importFrom purrr map
+read_example_input_files <- function()
+{
+  # Get the paths to the inp files
+  inp_files <- example_input_files()
+  
+  # Read the inp files
+  inputs <- purrr::map(inp_files, read_inp)
+  
+  # Name the list elements according to the file names
+  stats::setNames(inputs, basename(inp_files))
+}
+
+# existing_path_or_null --------------------------------------------------------
+existing_path_or_null <- function(x)
+{
+  if (length(x[1]) > 0L && file.exists(x[1L])) {
+    x
+  } else {
+    NULL
+  }
+}
