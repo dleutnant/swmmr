7652459c35dfc838726d0128826e261145d30a1e
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2023-01-11 06:55:43 +0100

    Use testthat::skip_on_ci()
    instead of skip_on_cran(), skip_on_travis().
    Put the calls back into the test_that() blocks.
    It seems that this is the intended way to do.

--- tests/testthat/test_swmm.R
+++ tests/testthat/test_swmm.R
@@ -1,11 +1,10 @@
 #source("./tests/testthat.R")
 #source("./tests/testthat/helpers.R")
 
-# only local tests
-testthat::skip_on_cran()
-testthat::skip_on_travis()
-
 testthat::test_that("swmm_io", {
+
+  # only local tests
+  testthat::skip_on_ci()
   
   # get the inp files
   inp_files <- swmmr:::example_input_files()
@@ -56,6 +55,9 @@ testthat::test_that("swmm_io", {
 
 testthat::test_that("rpt_reader", {
   
+  # only local tests
+  testthat::skip_on_ci()
+  
   # get the inp files
   inp_files <- swmmr:::example_input_files()
   
@@ -83,6 +85,9 @@ testthat::test_that("rpt_reader", {
 })
 
 testthat::test_that("swmm error", {
+  
+  # only local tests
+  testthat::skip_on_ci()
   
   # get the inp files
   inp_file <- swmmr:::example_input_files(ids = 1L)
@@ -124,6 +129,9 @@ testthat::test_that("swmm error", {
 })
 
 testthat::test_that("summary", {
+  
+  # only local tests
+  testthat::skip_on_ci()
   
   # get the inp files
   inp_files <- swmmr:::example_input_files()
