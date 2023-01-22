e9e817942a8ba819a02622ff81e18a9d54fd40be
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2023-01-22 12:20:55 +0100

    Clean list_of_sectoins_to_inp.R further
    - rename get_section_order_for_input() to
      get_section_names_for_input()
    - use intersect
    - remove hard-coded definition of column names

--- R/list_of_sections_to_inp.R
+++ R/list_of_sections_to_inp.R
@@ -28,69 +28,22 @@ list_of_sections_to_inp <- function(
     # Reclass to tibbles for consistency
     purrr::map( ~ set_class(.x, c("tbl_df", "tbl", "data.frame")))
   
-  # adjust order of sections
-  section_order <- get_section_order_for_input()
-  
-  result <- result[section_order[section_order %in% names(result)]]
+  # Select/order sections
+  # TODO: warn about sections that get lost here!
+  result <- result[intersect(get_section_names_for_input(), names(result))]
   
   # assign class attribute
   set_class(result, "inp")
 }
 
-# get_section_order_for_input ---------------------------------------------------
-get_section_order_for_input <- function()
+# get_section_names_for_input --------------------------------------------------
+get_section_names_for_input <- function()
 {
-  sections <- c(
-    "title", 
-    "options", 
-    "evaporation", 
-    "raingages", 
-    "subcatchments",
-    "subareas", 
-    "infiltration", 
-    "aquifers", 
-    "groundwater", 
-    "LID_controls", 
-    "LID_usage",
-    "junctions", 
-    "outfalls", 
-    "storage", 
-    "conduits", 
-    "pumps", 
-    "weirs", 
-    "xsections", 
-    "controls", 
-    "DWF", 
-    "pollutants", 
-    "landuses", 
-    "coverages", 
-    "loadings", 
-    "buildup", 
-    "washoff", 
-    "inflows", 
-    "timeseries", 
-    "curves", 
-    "patterns",
-    "report", 
-    "tags", 
-    "map", 
-    "coordinates", 
-    "vertices", 
-    "polygons", 
-    "labels", 
-    "symbols", 
-    "backdrop"
-  )
-
-  # Instead: read section names from "sections.csv"  
+  # Read section names from "sections.csv"  
   info <- swmmr:::section_info()
   info <- info[!is.na(info$input), ]
   
   # Order section names by number in column "input" first
   # and by the section name second
-  sections2 <- info$section[order(info$input, info$section)]
-  
-  stopifnot(identical(section2, tolower(sections)))
-  
-  sections
+  info$section[order(info$input, info$section)]
 }
