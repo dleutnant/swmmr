233de922a436615a207107d6994f07e84cae9f31
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2023-01-22 12:49:58 +0100

    Clean read_lid_rpt()
    - use "L" to indicate integer constants
    - store project and LID unit in variables (DRY)

--- R/read_lid_rpt.R
+++ R/read_lid_rpt.R
@@ -35,24 +35,27 @@ read_lid_rpt <- function(x, return_xts = TRUE, ...)
   )
   
   # parse project and LID Unit
-  meta_info <- readr::read_lines(file = x, skip = 2, n_max = 2) %>%
-    substr(10, nchar(.)) %>% 
+  meta_info <- readr::read_lines(file = x, skip = 2L, n_max = 2L) %>%
+    substr(10L, nchar(.)) %>% 
     trimws()
+  
+  project <- meta_info[1L]
+  lid_unit <- meta_info[2L]
   
   # get the data
   lid_rpt <- readr::read_table2(
     file = x,  
     col_names = header, 
     col_types = "ccddddddddddddd",
-    skip = 9, 
+    skip = 9L, 
     ...
   ) %>% 
     # make one datetime col
     tidyr::unite(col = "DateTime", Date, Time, sep = " ") %>% 
     dplyr::mutate(
       DateTime = as.POSIXct(DateTime, format = "%m/%d/%Y %H:%M:%S"), 
-      Project = meta_info[1], 
-      `LID Unit` = meta_info[2]
+      Project = project, 
+      `LID Unit` = lid_unit
     )
   
   # convert to xts
@@ -60,8 +63,8 @@ read_lid_rpt <- function(x, return_xts = TRUE, ...)
     lid_rpt <- xts::xts(
       x = dplyr::select(lid_rpt, -DateTime, -Project, -`LID Unit`),
       order.by = dplyr::pull(lid_rpt, DateTime),
-      Project = meta_info[1], 
-      `LID Unit` = meta_info[2]
+      Project = project, 
+      `LID Unit` = lid_unit
     )
   }
   
