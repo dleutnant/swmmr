997de23f3a4ff389b1cd026078bab504615cfd9c
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2023-01-22 20:09:49 +0100

    Clean read_rpt.R
    - indicate integer constants with "L"
    - improve variable names
    - use set_class()

--- R/read_rpt.R
+++ R/read_rpt.R
@@ -14,70 +14,81 @@
 read_rpt <- function(x, ...)
 {
   # read lines and trimws
-  rpt_lines <- readr::read_lines(x, ...) %>% 
+  report_lines <- readr::read_lines(x, ...) %>% 
     trimws(.) %>% 
     .[!grepl("---------", .)]
   
   # which sections are available?
   section_available <- purrr::map_lgl(
-    report_sections, ~ any(grepl(., x = rpt_lines))
+    report_sections, ~ any(grepl(., x = report_lines))
   )
   
   # last three lines contain analysis_info data
   # remove lines and add analysis_info to final list
-  idx_last_lines <- tail(seq_along(rpt_lines), 3)
-  analysis_info <- tibble::tibble(value = rpt_lines[idx_last_lines])
-  rpt_lines <- rpt_lines[-idx_last_lines]
+  idx_last_lines <- tail(seq_along(report_lines), 3L)
+  analysis_info <- tibble::tibble(value = report_lines[idx_last_lines])
+  report_lines <- report_lines[-idx_last_lines]
   
   # if no sections can be found, we got errors
   if (!any(section_available)) {
+    
     message("There are errors.")
-    res <- section_to_tbl(x = rpt_lines, section_name = "rpt_error")
-    res <- list(error = res, analysis_info = analysis_info)
-    # assign class attribute
-    class(res) <- "rpt_error"
-    return(res)
+    result <- section_to_tbl(x = report_lines, section_name = "rpt_error")
+    result <- list(error = result, analysis_info = analysis_info)
+    
+    # Return result with class attribute set
+    return(set_class(result, "rpt_error"))
   }
   
   # subset to available sections only
   report_sections <- report_sections[section_available] %>% 
     # sort vector
-    purrr::map_int(., ~ grep(., x = rpt_lines)) %>% 
+    purrr::map_int(., ~ grep(., x = report_lines)) %>% 
     purrr::set_names(report_sections[section_available]) %>% 
     sort %>% 
     names
   
   # find section start
   section_start <- report_sections %>%
-    purrr::map(~ grep(., x = rpt_lines) - 1) %>%
+    purrr::map(~ grep(., x = report_lines) - 1L) %>%
     purrr::map_if(
       ., 
-      ~ identical(., integer(0)) | identical(., numeric(0)), ~ NA
+      ~ identical(., integer(0L)) | identical(., numeric(0L)), 
+      ~ NA
     ) %>% 
     as.integer(.)
   
   # get end per section
-  section_end <- purrr::map(section_start[-1] - 3, function(x) {
+  section_end <- purrr::map(section_start[-1L] - 3L, function(x) {
+    
     # we need this only for sections before analysis options
-    if (startsWith(rpt_lines[x], "not just")) x <- x - 7
-    return(x)
+    if (startsWith(report_lines[x], "not just")) {
+      x <- x - 7L
+    }
+    
+    x
+    
   }) %>%
     # remove last three lines (analysis)
-    c(., length(rpt_lines) - 3) %>% 
-    as.integer(.)
+    c(., length(report_lines) - 3L) %>% 
+    as.integer()
   
   # occasionally, swmm produces less than 2 empty lines between sections
   for (i in seq_along(section_end)) {
+    
     x <- section_end[i]
+    
     # check if subsequent line is empty
-    while (!identical(rpt_lines[x + 1], "") & x < length(rpt_lines)) {
-      x <- x + 1  
+    while (!identical(report_lines[x + 1L], "") & x < length(report_lines)) {
+      x <- x + 1L
     }
+    
     section_end[i] <- x
   }
   
   # remove empty sections (and skip section name)
-  section_not_emtpy <- (section_end - section_start > 0)
+  section_not_emtpy <- (section_end - section_start > 0L)
+  
   section <- list(
     start = section_start[section_not_emtpy],
     end = section_end[section_not_emtpy], 
@@ -87,21 +98,19 @@ read_rpt <- function(x, ...)
   # create list with sections  
   list_of_sections <- section %>% 
     purrr::transpose() %>% 
-    purrr::map( ~ rpt_lines[.$start:.$end]) %>% 
+    purrr::map( ~ report_lines[.$start:.$end]) %>% 
     purrr::set_names(gsub("\\s+|-", "_", base::tolower(section$name)))
   
   # parse sections individually
-  res <- purrr::imap(list_of_sections, ~ section_to_tbl(.x, .y)) %>% 
+  result <- purrr::imap(list_of_sections, ~ section_to_tbl(.x, .y)) %>% 
     # discard nulls (nulls are returned if section is not parsed)
     purrr::discard(is.null) %>% 
     # discard empty tibbles (sections were parsed but empty)
-    purrr::discard( ~ nrow(.) < 1)
+    purrr::discard( ~ nrow(.) < 1L)
   
   # add analysis info
-  res$analysis_info <- analysis_info
+  result$analysis_info <- analysis_info
   
   # assign class attribute
-  class(res) <- "rpt"
-  
-  res
+  set_class(result, "rpt")
 }
